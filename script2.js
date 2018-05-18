466efd1b0238d16983ae5ed9476beda07ec77ee1
christineli0330 <36552148+christineli0330@users.noreply.github.com>
2018-05-18 15:13:01 -0700

    commit change
    update

--- script2.js
+++ script2.js
@@ -1,155 +1,54 @@
-var trialNumber = 0;
 var trialData = [];
-var itemA = 0;
-var itemB = 0;
-var ComparingTrial = 0;
 var productRanking = [];
+var sequence = 0;
 products.sort(function(){
 return 0.5 -Math.random()
 } )
 
 for (var i = 0; i < products.length; i++) {
-
      productRanking.push({
       itemRanking: products[i],
      })
 }
-//shuffle(products);
-//function choosePic() {
-//var randomNum = Math.floor(Math.random() * myPictures.length);
-//     document.getElementById("myPicture").src = myPictures[randomNum];}
 
 function pageLoad(){
-
 	document.getElementById('consent').style.display = 'block';
-
 }
+
 function clickConsent(){
 	document.getElementById('consent').style.display = 'none';
 	document.getElementById('instructions').style.display = 'block';
-
 }
 
 function clickInstructions(){
 	document.getElementById('instructions').style.display = 'none';
 	ranking();
-
-}
-
-
-function trialStart(){
-	// take next product, and put in the product description.
-//	choosePic();
-document.getElementById('trialText').innerHTML = products[trialNumber].name;
-document.getElementById("myPicture").src = products[trialNumber].img;
-	// reset the slider
-	document.getElementById('trialSlider').value = 500;
-
-	document.getElementById('trial').style.display = 'block';
-document.getElementById('next').disabled=true;;
-document.getElementById('next').style.backgroundColor="grey";
-}
-
-function trialDone(){
-	document.getElementById('trial').style.display = 'none';
-	// record what the subject said
-	trialData.push({
-		trialNumber: trialNumber,
-		product: products[trialNumber],
-		response: document.getElementById('trialSlider').value});
-	// increment the trialNumber
-	trialNumber = trialNumber+1;
-
-	// if we are done with all trials, then go to completed page
-	if(trialNumber >= products.length){
-		document.getElementById('completed').style.display = 'block';
-	} else {
-	// if we are not done with all trials, then show the next trial.
-		trialStart();
-	}
-
-}
-
-function comparingStart(){
-  //randomly select two of the products
-	//put into one question with silde bar
-	itemA = Math.floor(Math.random() * 54);
-	document.getElementById('Text1').innerHTML = products[itemA].name;
-	document.getElementById("Picture1").src = products[itemA].img;
-		// reset the slider
-		itemB = Math.floor(Math.random() * 54);
-		document.getElementById('Text2').innerHTML = products[itemB].name;
-		document.getElementById("Picture2").src = products[itemB].img;
-
-		document.getElementById('CompSlider').value = 500;
-
-		document.getElementById('Comparing').style.display = 'block';
-	  document.getElementById('nextitem').disabled=true;;
-	  document.getElementById('nextitem').style.backgroundColor="grey";
-}
-
-//record the data in a right way
-//end the loop
-function comparingDone() {
-	document.getElementById('Comparing').style.display = 'none';
-
-	//need to figure out how to record the data precisely
-	trialData.push({
-		ComparingTrial: ComparingTrial,
-		itemA: products[itemA],
-		itemB: products[itemB],
-		ComparingResponse: document.getElementById('trialSlider').value});
-	ComparingTrial = ComparingTrial + 1;
-	if (ComparingTrial >= 5) {
-		trialStart();
-		}
-		else {
-		comparingStart();
-	}
 }
 
 function ranking() {
+  $('#sortable').html('');
+  for(i = 0; i<10; i++){
+    current_product = Math.floor(Math.random() * productRanking.length);
+    $("#sortable").append("<li id='"+productRanking[current_product].itemRanking.img+"'>"+productRanking[current_product].itemRanking.name+"</li>")
+    productRanking.splice(current_product,1);
+  }
   document.getElementById('rankingTrial').style.display = 'block';
-  Trail = Math.floor(Math.random() * productRanking.length);
-  document.getElementById('item1').innerHTML = productRanking[Trail].itemRanking.name;
-  productRanking.splice(Trail,1);
-Trail = Math.floor(Math.random() * productRanking.length);
-  document.getElementById('item2').innerHTML = productRanking[Trail].itemRanking.name;
-productRanking.splice(Trail,1);
-Trail = Math.floor(Math.random() * productRanking.length);
-document.getElementById('item3').innerHTML = productRanking[Trail].itemRanking.name;
-productRanking.splice(Trail,1);
-Trail = Math.floor(Math.random() * productRanking.length);
-document.getElementById('item4').innerHTML = productRanking[Trail].itemRanking.name;
-productRanking.splice(Trail,1);
-Trail = Math.floor(Math.random() * productRanking.length);
-document.getElementById('item5').innerHTML = productRanking[Trail].itemRanking.name;
-productRanking.splice(Trail,1);
-Trail = Math.floor(Math.random() * productRanking.length);
-document.getElementById('item6').innerHTML = productRanking[Trail].itemRanking.name;
-productRanking.splice(Trail,1);
-Trail = Math.floor(Math.random() * productRanking.length);
-document.getElementById('item7').innerHTML = productRanking[Trail].itemRanking.name;
-productRanking.splice(Trail,1);
-Trail = Math.floor(Math.random() * productRanking.length);
-document.getElementById('item8').innerHTML = productRanking[Trail].itemRanking.name;
-productRanking.splice(Trail,1);
-Trail = Math.floor(Math.random() * productRanking.length);
-document.getElementById('item9').innerHTML = productRanking[Trail].itemRanking.name;
-productRanking.splice(Trail,1);
-
 }
 
 function rankingDone() {
-	document.getElementById('rankingTrial').style.display = 'none';
+ document.getElementById('rankingTrial').style.display = 'none';
  //record data
-  //trialData.push({
-
-	//})
- experimentDone();
-
+ trialData.push({
+ ranking: $( "#sortable" ).sortable( "toArray" ),
+	})
+  sequence = sequence+1;
+  if (sequence < 3){
+    ranking()
+  }
+  else {
+    document.getElementById('completed').style.display = 'block';
+  }
 }
-
 
 function experimentDone(){
 	window.location = 'http://www.evullab.org';
